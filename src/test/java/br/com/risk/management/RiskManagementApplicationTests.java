48102f4781d02203340744f5fba3e3004460b790
@alessandro-cav <alessandrocav14@gmail.com>
2022-11-13 13:04:31 -0300

    Criando o projeto risk management no repositorio

---
+++ src/test/java/br/com/risk/management/RiskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.risk.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
